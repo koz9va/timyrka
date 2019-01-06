04badfcc1815ebfe9abab6853e815ce874d73773
Andrey Grinenko <32901306+Kushhh@users.noreply.github.com>
2019-01-06 22:40:05 +0300

    Clear

--- main.py
+++ main.py
@@ -5,66 +5,51 @@ from telegram.ext import Updater, CommandHandler
 from bs4 import BeautifulSoup
 import requests
 
+updater = Updater('771027063:AAHjnTSc5uH5BapuPAHsHwuKiN7VaQludzc') # токен бота
+
 class Model(): # класс в котором храннятся все данные которые мы передаём пользователям
     timer = int()
     # в следующей строке делаем запрос на сайт с количеством игроков и скрапим его через beautiful soup
     pubg_site = BeautifulSoup(requests.get('https://steamcharts.com/app/578080').text, features="html.parser").find_all('span', class_='num')[0].contents[0]
-<<<<<<< HEAD
-    # helloFile = open('hello.txt', 'r')
-    # hellodata = helloFile.readlines() # открывем файл и создаём массив со строками приветствия
-    # helloFile.close()
-=======
     btcusd = BeautifulSoup(requests.get('https://altstake.io').text, features="html.parser").find_all('strong', class_='down')[0].contents[0]
     helloFile = open('hello.txt', 'r')
     hellodata = helloFile.readlines() # открывем файл и создаём массив со строками приветствия
     helloFile.close()
->>>>>>> master
     def updatesoap(self): # метод который обновляет количество игроков
         self.pubg_site = BeautifulSoup(requests.get('https://steamcharts.com/app/578080').text, features="html.parser").find_all('span', class_='num')[0].contents[0]
     def getAmount(self):
         return self.pubg_site
-<<<<<<< HEAD
-
-
-=======
     def updbtc(self):
-        self.btcusd = BeautifulSoup(requests.get('https://altstake.io').text, features="html.parser").find_all('strong', class_='down')[0].contents[0]    
+        self.btcusd = BeautifulSoup(requests.get('https://altstake.io').text, features="html.parser").find_all('strong', class_='down')[0].contents[0]
     def getAmountBTC(self):
-        return self.btcusd    
+        return self.btcusd
 
->>>>>>> master
 model = Model() # создаём модель
+
 
 def hello(bot, update):
     rn = random.randint(0, len(model.hellodata)) # генерируем случайный номер строки
-    update.message.reply_text(
-        'Привет, {}!'.format(update.message.from_user.first_name)
-    )
+    update.message.reply_text('Привет, {}!'.format(update.message.from_user.first_name))
     bot.sendMessage(chat_id=update.message.chat_id, text=model.hellodata[rn]) # отправляем случайный елемент масива
-updater = Updater('771027063:AAHjnTSc5uH5BapuPAHsHwuKiN7VaQludzc') # токен бота
+
 def pubg(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text='Текущий онлайн в PUBG: ' + str(model.getAmount()))
 
 def joke(bot, update):
-<<<<<<< HEAD
     soup = BeautifulSoup(requests.get('https://randstuff.ru/joke/').text, features="html.parser")
     joke = soup.find(class_="text").contents[0].contents[0].contents[0]
     bot.sendMessage(chat_id=update.message.chat_id, text=str(joke))
-=======
-    bot.sendMessage(chat_id=update.message.chat_id, text='Ебанная шутка')
 
 def btc(bot, update):
     round(getAmountBTC, 2)
-    bot.sendMessage(chat_id=update.message.chat_id, text='BTC/USD: ' + str(model.getAmountBTC()))    
->>>>>>> master
+    bot.sendMessage(chat_id=update.message.chat_id, text='BTC/USD: ' + str(model.getAmountBTC()))
+
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(CommandHandler('pubg', pubg))
 updater.dispatcher.add_handler(CommandHandler('joke', joke))
-<<<<<<< HEAD
-=======
 updater.dispatcher.add_handler(CommandHandler('btc', btc))
->>>>>>> master
+
 
 def callbacks():
     model.updatesoap()# функция которая вызывется раз в пять минут в которая запускает все остальные функции
