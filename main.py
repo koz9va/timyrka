bcffc104c80f76a674bc41574020e39eeb22a8f7
Andrey Grinenko <32901306+Kushhh@users.noreply.github.com>
2019-01-06 18:32:50 +0300

    test

--- main.py
+++ main.py
@@ -1,36 +1,55 @@
+import random
 import logging
 import time, threading
 from telegram.ext import Updater, CommandHandler
 from bs4 import BeautifulSoup
 import requests
 
-class Model():
+class Model(): # класс в котором храннятся все данные которые мы передаём пользователям
     timer = int()
+    # в следующей строке делаем запрос на сайт с количеством игроков и скрапим его через beautiful soup
     pubg_site = BeautifulSoup(requests.get('https://steamcharts.com/app/578080').text, features="html.parser").find_all('span', class_='num')[0].contents[0]
-    def updatesoap(self):
+    # helloFile = open('hello.txt', 'r')
+    # hellodata = helloFile.readlines() # открывем файл и создаём массив со строками приветствия
+    # helloFile.close()
+    def updatesoap(self): # метод который обновляет количество игроков
         self.pubg_site = BeautifulSoup(requests.get('https://steamcharts.com/app/578080').text, features="html.parser").find_all('span', class_='num')[0].contents[0]
     def getAmount(self):
         return self.pubg_site
-            
 
-model = Model()
+class Joke(): # класс в котором храннятся все данные которые мы передаём пользователям
+    # в следующей строке делаем запрос на сайт с количеством игроков и скрапим его через beautiful soup
+    pubg = BeautifulSoup(requests.get('https://steamcharts.com/app/578080').text, features="html.parser").find_all('span', class_='num')[0].contents[0]
+    def get(self):
+        return self.pubg
+
+
+
+model = Model() # создаём модель
+joke = Joke()
+
 
 def hello(bot, update):
+    rn = random.randint(0, len(model.hellodata)) # генерируем случайный номер строки
     update.message.reply_text(
         'Привет, {}!'.format(update.message.from_user.first_name)
     )
-    bot.sendMessage(chat_id=update.message.chat_id, text='Я бот-долбобот и пока что тупой как пробка')
-updater = Updater('TOKEN')
+    bot.sendMessage(chat_id=update.message.chat_id, text=model.hellodata[rn]) # отправляем случайный елемент масива
+updater = Updater('771027063:AAHjnTSc5uH5BapuPAHsHwuKiN7VaQludzc') # токен бота
 def pubg(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text='Текущий онлайн в PUBG: ' + str(model.getAmount()))
 
+def joke(joke, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text='98')
+
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(CommandHandler('pubg', pubg))
-def callbacks():
-    model.updatesoap()
-    threading.Timer(300 , callbacks).start()
-if __name__ == '__main__':
-    callbacks()
-    updater.start_polling()
-    updater.idle()
+updater.dispatcher.add_handler(CommandHandler('joke', joke))
 
+def callbacks():
+    model.updatesoap()# функция которая вызывется раз в пять минут в которая запускает все остальные функции
+
+if __name__ == '__main__': # проверка на прямой запуск файла, то есть если добавить его через import эти комманды не будут выполнены
+    threading.Timer(300 , callbacks).start()# запуск периодических функцый в отдельном потоке
+    updater.start_polling() # сам запуск бота
+    updater.idle()
