85df1e3a39689971b72cd498f770a07cd85d4d97
koz9va <serafim597@gmail.com>
2019-01-07 20:08:02 +0200

    fixed /btc
    added json function

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import time, threading
 from telegram.ext import Updater, CommandHandler
 from bs4 import BeautifulSoup
 import requests
-
+def getJsonVal(link = str(), path = list()): # функция которая принимает строку ссылки и "путь" к данным в json
+        jsonResponce = requests.get(link).json() # а потом возваращет готовый результат
+        for step in path: # нужно подумать что делать в том случае если на пути будет массив
+            jsonResponce = jsonResponce[step]
+        return jsonResponce
 class Model(): # класс в котором храннятся все данные которые мы передаём пользователям
     timer = int()
     # в следующей строке делаем запрос на сайт с количеством игроков и скрапим его через beautiful soup
     pubg_site = BeautifulSoup(requests.get('https://steamcharts.com/app/578080').text, features="html.parser").find_all('span', class_='num')[0].contents[0]
-    btcusd = BeautifulSoup(requests.get('https://altstake.io').text, features="html.parser").find_all('strong', class_='down')[0].contents[0]
+    btcusd = getJsonVal('https://api.coindesk.com/v1/bpi/currentprice.json', ('bpi', 'USD', 'rate'))
     helloFile = open('hello.txt', 'r')
     hellodata = helloFile.readlines() # открывем файл и создаём массив со строками приветствия
     helloFile.close()
@@ -18,7 +22,7 @@ class Model(): # класс в котором храннятся все данн
     def getAmount(self):
         return self.pubg_site
     def updbtc(self):
-        self.btcusd = BeautifulSoup(requests.get('https://altstake.io').text, features="html.parser").find_all('strong', class_='down')[0].contents[0]
+        self.btcusd = getJsonVal('https://api.coindesk.com/v1/bpi/currentprice.json', ('bpi', 'USD', 'rate'))
     def getAmountBTC(self):
         return self.btcusd
 
@@ -59,7 +63,7 @@ updater.dispatcher.add_handler(CommandHandler('btc', btc))
 
 def callbacks():
     model.updatesoap()# функция которая вызывется раз в пять минут в которая запускает все остальные функции
-
+    model.updbtc()
 if __name__ == '__main__': # проверка на прямой запуск файла, то есть если добавить его через import эти комманды не будут выполнены
     threading.Timer(300 , callbacks).start()# запуск периодических функцый в отдельном потоке
     updater.start_polling() # сам запуск бота
